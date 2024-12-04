77b84dba0bce131fcacb43512fc7bebe52d75a49
dealbertt <dealbertignacio@gmail.com>
2024-12-04 22:59:47 +0100

    Started Image Processing Proyect

---
+++ Image/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define IMAGE_NAME "sample.jpg"
+
+//we first work with an array of pixels  that hold the value of the gray scale(not colors yet)
+int main(){
+    FILE *ptr = fopen(IMAGE_NAME,"r");    
+
+}
