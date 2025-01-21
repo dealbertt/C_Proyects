e978ca08d0d7e70f44c63c005bbf302ae03f9640
dealbertt <dealbertignacio@gmail.com>
2025-01-21 20:06:32 +0100

    Worked around timer and keyboard

--- 03_Arcanoid/main.c
+++ 03_Arcanoid/main.c
@@ -1,4 +1,5 @@
 #include <SDL2/SDL_events.h>
+#include <SDL2/SDL_keycode.h>
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_video.h>
@@ -10,24 +11,25 @@
 #include <stdbool.h>
 
 
-SDL_Window *window;
-SDL_Surface *surface;
-void initGame(SDL_Window *window, SDL_Surface *surface);
-bool handleKeyboard(int x,int y);
+void initGame(SDL_Window **window, SDL_Surface **surface);
+bool handleKeyboard(SDL_Window *window,SDL_Surface *surface,int *x, int *y,short *timer);
+bool timer(short *timer);
+void resetTimer(short *timer);
 int main(){
+    SDL_Window *window = NULL;
+    SDL_Surface *surface = NULL;
+
 
     //Draw borders and place pad in the default place
-    initGame(window,surface);
+    initGame(&window,&surface);
 
+    int x = 50;
+
+    int y = 960;
     int i = 1;
-    int x = 0;
-    int y = 0;
-    while(handleKeyboard(x,y) != true){
-        clearPad(window, surface, x, y);
-        handleKeyboard(x, y);
-        printf("Outside handling\n");
-        printf("X: %d Y: %d\n",x,y);
-        drawPad(window,surface,x,y);
+    short timer = 5;
+    while(!handleKeyboard(window,surface,&x,&y,&timer)){
+        printf("Timer %d\n",timer);
     } 
     SDL_Quit();
     return 0;
@@ -35,41 +37,58 @@ int main(){
 
 }
 
-void initGame(SDL_Window *window, SDL_Surface *surface){
+void initGame(SDL_Window **window, SDL_Surface **surface){
 
     SDL_Init(SDL_INIT_VIDEO);
-    window = SDL_CreateWindow("Arcanoid",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1000,1000,0);
-    surface = SDL_GetWindowSurface(window);
+    *window = SDL_CreateWindow("Arcanoid",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1000,1000,0);
+    *surface = SDL_GetWindowSurface(*window);
     //drawPad(window, surface);
-    drawPad(window,surface,500,960);
-    drawBorders(window, surface);
+    drawBorders(*window, *surface);
+    drawPad(*window,*surface,500,960);
     return;
 
 }   
 
 
-bool handleKeyboard(int x, int y){
-    printf("Inside handling\n");
+bool handleKeyboard(SDL_Window *window,SDL_Surface *surface,int *x, int *y,short *time){
      bool quit = false;
-     SDL_Event event = event;
-     SDL_WaitEvent(&event);
-     event.type = SDL_KEYDOWN;
-                 switch (event.key.keysym.sym) {
-                     case SDLK_ESCAPE:
-                         quit = true;
-                         return quit;
-                    case SDLK_d:
-                         //move pad right
+     SDL_Event event;
+     //SDL_WaitEvent(&event);
+     SDL_PollEvent(&event);
+     if(timer(time)){
 
-                         clearPad(window, surface, 500, 960);
-                         x = x + 50;
-                         printf("D Key pressed\n");
-                         return quit;
-                    case SDLK_a:
-                         //move pad left
-                         x = x - 50;
-                         return quit;
-                 }
-         
+         if(event.type == SDL_KEYDOWN){
+             if(event.key.keysym.sym == SDLK_ESCAPE){
+                 quit = true;
+                 return quit;
+             }else if(event.key.keysym.sym == SDLK_d){
+                 clearPad(window, surface, *x, *y);
+                 *x += 50;
+                 drawPad(window,surface,*x,*y);
+
+             }else if(event.key.keysym.sym == SDLK_a){
+                 clearPad(window, surface, *x, *y);
+                 *x -= 50;
+                 drawPad(window,surface,*x,*y);
+             }
+
+             resetTimer(time);
+         }
+     }
+
      return quit;
+}
+bool timer(short *timer){
+    if(*timer <= 0){
+        printf("TRUE\n");
+        return true;
+    }else{
+        (*timer)--;
+        return false;
+    }
+
+}
+void resetTimer(short *timer){
+    *timer = 5;
+    return;
 }
