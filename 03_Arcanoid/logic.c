080d11c2a96c44bb100dd55b07ad331165a2905d
dealbertt <dealbertignacio@gmail.com>
2025-01-24 16:22:31 +0100

    Typeshe

--- 03_Arcanoid/logic.c
+++ 03_Arcanoid/logic.c
@@ -5,13 +5,13 @@ short BALL_TIMER = 50;
 const short PAD_TIMER_RESET = 50;
 bool timer(short *timer){
         printf("timer %hi\n",PAD_TIMER);
-    if(*timer <= 0){
-        printf("TRUE\n");
-        return true;
-    }else{
-        *timer = *timer - 1;
-        return false;
-    }
+        if(*timer <= 0){
+            printf("TRUE\n");
+            return true;
+        }else{
+            *timer = *timer - 1;
+            return false;
+        }
 
 }
 void resetTimer(const short initialValue,short *actualValue){
