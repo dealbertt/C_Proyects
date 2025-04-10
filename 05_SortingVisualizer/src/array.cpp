9463d24163d0e551b056ebca554f834bfee68c74
dealbertt <dealbertignacio@gmail.com>
2025-04-10 18:25:43 +0200

    Red color when traveling the array

--- 05_SortingVisualizer/src/array.cpp
+++ 05_SortingVisualizer/src/array.cpp
@@ -19,16 +19,30 @@ int initializeArray(SDL_Window *window){
 
     int x = 0;
 
+    int width = config->windowWidth / config->numberElements;
+    std::cout << "Optimum width i guess: " << width << std::endl;
+
     SDL_Surface *surface = SDL_GetWindowSurface(window);
     for(int i = 0; i < config->numberElements; i++){
         int guess = dist(gen);
-        array[i].rect  = {x, config->windowHeigth - guess, 15, guess};
+        array[i].rect  = {x, config->windowHeigth - guess, 8, guess};
         SDL_FillSurfaceRect(surface, &array[i].rect, 0xFFFFFF);
         SDL_UpdateWindowSurface(window);
 
-        x += 17;
+        x += (width + 2);
     }
-    sleep(2);
+
+    for(int i = 0; i < config->numberElements; i++){
+        SDL_FillSurfaceRect(surface, &array[i].rect, 0xFFFF0000);
+        SDL_UpdateWindowSurface(window);
+        SDL_Delay(50);
+
+        SDL_FillSurfaceRect(surface, &array[i].rect, 0xFFFFFFFF);
+        SDL_UpdateWindowSurface(window);
+        x += (width + 2);
+    }
+    sleep(4);
 
     return 0;
 }
+
